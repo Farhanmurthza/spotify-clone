6cf2e27eefbf1beb548c95a2dead6c4afb5d7ba1
Muhammad Farhan <paanmurthza@gmail.com>
2024-02-07 16:03:39 +0800

    stripe integrate

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,8 @@ import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 
 interface SideBarProps {
   children: React.ReactNode;
@@ -16,6 +18,7 @@ interface SideBarProps {
 
 const Sidebar: React.FC<SideBarProps> = ({ children, songs }) => {
   const pathname = usePathname();
+  const player = usePlayer();
 
   const routes = useMemo(
     () => [
@@ -36,7 +39,12 @@ const Sidebar: React.FC<SideBarProps> = ({ children, songs }) => {
   );
 
   return (
-    <div className="flex h-full">
+    <div
+      className={twMerge(
+        `flex h-full`,
+        player.activeId && "h-[calc(100% - 80px)]"
+      )}
+    >
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
@@ -46,7 +54,7 @@ const Sidebar: React.FC<SideBarProps> = ({ children, songs }) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          <Library songs={songs}/>
+          <Library songs={songs} />
         </Box>
       </div>
       <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
